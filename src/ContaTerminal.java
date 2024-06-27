9e8e8b559aeae8e017634ccd845d2557d8c5aebc
José Neilson <joseneilson13@gmail.com>
2024-06-27 20:10:51 -0300

    feat: add source-code for the challenge

---
+++ src/ContaTerminal.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int conta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        String mensagem;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor digite o número da agência");
+        agencia = scanner.next();
+
+        System.out.println("Por favor digite o número da conta");
+        conta = scanner.nextInt();
+
+        System.out.println("Por favor digite o nome do cliente");
+        nomeCliente = scanner.next();
+
+        System.out.println("Por favor digite o saldo da conta");
+        saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        mensagem = "Olá " + nomeCliente;
+        mensagem = mensagem.concat(", obrigado por criar uma conta em nosso banco, sua agência é " + agencia);
+        mensagem = mensagem.concat(", conta " + conta);
+        mensagem = mensagem.concat(" e seu saldo " + saldo);
+        mensagem = mensagem.concat(" já está disponível para saque");
+
+        System.out.println(mensagem);
+    }
+}
